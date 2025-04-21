4ea8db00eeaa788fbe20688758c86b76e715d974
clew2027 <clew27@seas.upenn.edu>
2025-04-20 20:57:45 -0400

    created files and restaurant struct

---
+++ Models/Restaurant.swift
@@ -0,0 +1,20 @@
+//
+//  Restaurant.swift
+//  
+//
+//  Created by Charlotte Lew on 4/20/25.
+//
+
+public struct Restaurant {
+    public var name: String
+    public var latitude: Double
+    public var longitude: Double
+    public var numDollarSigns: Int
+    
+    public var cuisine: String
+    public var popularDishes: [String]
+    public var atmosphere: String
+    public var parking: BooleanLiteralType
+    
+}
+
